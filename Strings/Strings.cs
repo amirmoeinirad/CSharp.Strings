79c5a88fa45320ee046787781e231a822d6aa821
Amir Moeini Rad <moeini@shabakehafzar.com>
2025-08-31 12:32:53 +0330

    First Commit

--- Strings/Strings.cs
+++ Strings/Strings.cs
@@ -3,6 +3,8 @@
 // August 2025
 
 // Main Concept: Strings in C#
+// String methods in this example: Length, CompareTo, Equals, ToLower, IndexOf, Split, Substring, Trim
+// StringBuilder class and its methods: Append, Insert, Capacity, MaxCapacity
 
 using System;
 using System.Text;
@@ -26,87 +28,98 @@ namespace Strings
 
 
             Console.WriteLine("Length of s1: {0} is {1} characters.", s1, s1.Length);
-            Console.WriteLine("Comparison result for {0} and {1}: {2}", s1, s2, s1.CompareTo(s2));
-            Console.WriteLine("Equality checking of {0} and {1}: {2}", s1, s3, s1.Equals(s3));
-            Console.WriteLine("Equality checking of {0} with lower case {1} ({2}): {3}", s3, s1, s1.ToLower(), s3.Equals(s1.ToLower()));
-            Console.WriteLine("The index of A in {0} is: {1}", s1, s1.IndexOf("A"));
-            Console.WriteLine("\nThe individual words of '{0}' are:", s4);
-
-
+            Console.WriteLine("Comparison result for s1: {0} and s2: {1}: {2}", s1, s2, s1.CompareTo(s2));
+            Console.WriteLine("Equality checking of s1: {0} and s2: {1}: {2}", s1, s3, s1.Equals(s3));
+            Console.WriteLine("Equality checking of s3: {0} with lower case s1: {1} ({2}): {3}", s3, s1, s1.ToLower(), s3.Equals(s1.ToLower()));
+            Console.WriteLine("The index of 'A' in s1: {0} is: {1}", s1, s1.IndexOf("A"));
+            
+            
+            Console.WriteLine("\nThe individual words of s4: '{0}' are:", s4);
             string[] words = s4.Split();
             foreach (string word in words)
             {
-                Console.WriteLine(" {0}",word);
+                Console.WriteLine("{0}", word);
             }
 
-            Console.WriteLine("\nThe substring of '{0}' fron index 3 of length 10 is:\n {1}", s4, s4.Substring(3, 10));
-            Console.WriteLine("\nThe string '{0}' after trimming becomes\n'{1}'", s5, s5.Trim());
+
+            Console.WriteLine("\nThe substring of s4: '{0}' from index 3 with a length of 10 is:\n {1}", s4, s4.Substring(3, 10));
+            
+            
+            Console.WriteLine("\nThe string s5: '{0}' after trimming becomes\n'{1}'", s5, s5.Trim());
+
 
             // You can't change or modify a string.
+            // Strings are immutable.
             // string myStr = "abcd";
             // myStr[1] = "e";
 
+
             Console.WriteLine();
 
+
             // Using a quote in a string
-
             Console.WriteLine("\"Hello World!\"");
-            
-            // Using a backslash in a string
 
+
+            // Using a backslash in a string
+            // Method 1: You need to escape it with another backslash.
             Console.WriteLine("My Code: C:\\Books\\TYCSharp\\Originals\\");
 
-            // A verbatim string literal
 
+            // A verbatim string literal
+            // Method 2: Use @ before the string.
             Console.WriteLine(@"My Code: C:\Books\TYCSharp\Originals\");
             Console.WriteLine(@"""Hello World!""");
 
-            // Manipulating strings
 
+            // Manipulating strings
+            // The 'StringBuilder' class helps you to manipulate strings.
             StringBuilder name = new StringBuilder();
             string buffer;
             int marker = 0;
 
             Console.Write("\nEnter your first name: ");
             buffer = Console.ReadLine();
-
-            if ( buffer != null )
+            if (buffer != null)
             {
                 name.Append(buffer);
                 marker = name.Length;
             }
 
-            Console.Write("\nEnter your last name: ");
+            Console.Write("Enter your last name: ");
             buffer = Console.ReadLine();
-
-            if ( buffer != null )
+            if (buffer != null)
             {
                 name.Append(" ");
                 name.Append(buffer);
             }
 
-            Console.Write("\nEnter your middle name: ");
+            Console.Write("Enter your middle name: ");
             buffer = Console.ReadLine();
-
-            if ( buffer != null )
+            if (buffer != null)
             {
-                name.Insert(marker+1, buffer);
-                name.Insert(marker+1+buffer.Length, " ");
+                name.Insert(marker + 1, buffer);
+                name.Insert(marker + 1 + buffer.Length, " ");
             }
 
-            Console.WriteLine("\n\nFull name: {0}", name);
+            Console.WriteLine("Full name: {0}", name);
 
-            // Some stats....
-
-            Console.WriteLine("\n\nInfo about StringBuilder string:");
+            
+            Console.WriteLine("\nInfo about the StringBuilder string:");
             Console.WriteLine("value: {0}", name);
+
+            // StringBuilder.Capacity gets or sets the maximum number of characters that can be contained in the memory
+            // allocated by the current instance of StringBuilder.
             Console.WriteLine("Capacity: {0}", name.Capacity);
+
+            // StringBuilder.MaxCapacity gets the maximum capacity of this instance of StringBuilder.
             Console.WriteLine("Maximum Capacity: {0}", name.MaxCapacity);
+
+            // StringBuilder.Length gets or sets the length of the current StringBuilder object.
             Console.WriteLine("Length: {0}", name.Length);
 
-            Console.WriteLine();
+
+            Console.WriteLine("\nDone.");
         }
     }
 }
-
-// Strings or strings (String class objects) are immutable by nature.
